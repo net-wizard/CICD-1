9d204b7cc9f933bc6a8852474b9e9b65049df522
Shubham Thorat <shubhamthorat2202@gmail.com>
2021-07-27 16:25:07 +0530

    Changed My website to Dynamic '{{title}}'

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", title="Shubham Thorat")
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="Shubham Thorat")
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", title="Shubham Thorat")
 
 
 
